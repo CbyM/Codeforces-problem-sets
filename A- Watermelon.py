03e4339849398cfb198d8f3032d46f8099590214
P. Misirov <47452703+CbyM@users.noreply.github.com>
2020-05-26 11:26:13 +0200

    Add files via upload

---
+++ A- Watermelon.py
@@ -0,0 +1,13 @@
+# https://codeforces.com/contest/4/problem/A
+# Verdict: Accepted
+# Memory: 0 KB
+# Time: 186 ms
+# CbyM
+
+
+
+w = int(input())
+
+if w % 2 == 0 and w > 2:
+    print('yes')
+else: print('no')
